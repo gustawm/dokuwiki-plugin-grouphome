ac5dca5ad2c7459732f4281310ab0d24bb864880
gustawm <gustaw@data.pl>
2013-03-13 02:45:55 +0100

    This mod work with dokuwiki "Adora Belle"

--- action.php
+++ action.php
@@ -4,45 +4,40 @@
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Andreas Gohr <gohr@cosmocode.de>
- */
+ * @modifier Lukasz Zalewski <lukasz@lukaszzalewski.com>
+*/
 
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 
 require_once DOKU_PLUGIN.'action.php';
 
 class action_plugin_grouphome extends DokuWiki_Action_Plugin {
-
-    public function register(Doku_Event_Handler &$controller) {
-
-       $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_hook');
-
-    }
-
-    public function handle_hook(Doku_Event &$event, $param) {
+    function handle_hook(Doku_Event &$event, $param) {
+        global $conf;
         global $INFO;
         global $ID;
-        global $conf;
+        if (($_SERVER['REMOTE_USER']!=null)&&($_REQUEST['do']=='login')) {
+            if($ID != $conf['start']) return;
+            if(act_clean($event->data) != 'login') return;
+            $grps = (array) $INFO['userinfo']['grps'];
+            if(!count($grps)) return;
+            
+            // get the new namespace of start page
+            $pages = $this->getConf('grouppages');
 
-        if($ID != $conf['start']) return;
-        if(act_clean($event->data) != 'show') return;
-
-        $grps = (array) $INFO['userinfo']['grps'];
-        if(!count($grps)) return;
-
-        $pages = $this->getConf('grouppages');
-
-        foreach($grps as $grp){
-            $page = cleanID(sprintf($pages,$grp));
-            if(page_exists($page)){
-                send_redirect(wl($page,'',true));
+            foreach($grps as $grp){
+                $page = cleanID(sprintf($pages,$grp));
+                if(page_exists($page)){
+                    send_redirect(wl($page,'',true));
+                }
             }
         }
     }
-}
 
-// vim:ts=4:sw=4:et:
+    function register(&$controller) {
+        $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_hook');
+    }
+}
